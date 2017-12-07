236c2087173eea557595775fdfac84a2385fafaa
zozoz <zozozonbi@gmail.com>
2017-12-07 19:58:37 +0900

    POST通信に変更しました。

--- src/main/java/com/internouslab/application/api/UserInfoApi.java
+++ src/main/java/com/internouslab/application/api/UserInfoApi.java
@@ -1,23 +1,26 @@
 package com.internouslab.application.api;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.internouslab.application.entity.UserInfoEntity;
 import com.internouslab.application.service.UserInfoServiceInterface;
 
 @RestController
-@RequestMapping("/InternousLabServer/UserInfo")
+@RequestMapping("/InternousLabServer/UserInfo/")
 public class UserInfoApi {
 	
 	@Autowired
 	UserInfoServiceInterface userInfoServiceImpl;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public UserInfoEntity getUserInfo(@RequestParam(value = "userName") String userName, @RequestParam(value = "password") String password) {
+	@RequestMapping(method = RequestMethod.POST)
+	public UserInfoEntity getUserInfo(HttpServletRequest request) {
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
 		return userInfoServiceImpl.findByUserNameAndPassword(userName, password);
 	}
 }
